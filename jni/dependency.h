1e12bb2502f1870923cf4fcbe260ca911c1fb1c3
roman10 <liuf0005@gmail.com>
2011-12-05 12:36:40 +0800

    add project.properties

--- jni/dependency.h
+++ jni/dependency.h
@@ -115,5 +115,8 @@ void dep_decode_a_video_packet(int p_videoFileIndex);
 int load_gop_info(FILE* p_gopRecFile, int *p_startF, int *p_endF);
 int if_dependency_complete(int p_videoFileIndex, int p_gopNum);
 void prepare_decode_of_gop(int p_videoFileIndex, int _stFrame, int _edFrame, int _roiSh, int _roiSw, int _roiEh, int _roiEw);
-
+void load_frame_mb_stindex(int p_videoFileIndex);
+void load_frame_mb_edindex(int p_videoFileIndex);
+void unload_frame_mb_stindex(void);
+void unload_frame_mb_edindex(void);
 
