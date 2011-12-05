c28f436f54b0f355d02bacadecf82d5f7effe80e
roman10 <liuf0005@gmail.com>
2011-12-05 12:37:45 +0800

    add missing files

---
+++ src/feipeng/andzop/utils/FileUtilsStatic.java
@@ -0,0 +1,19 @@
+package feipeng.andzop.utils;
+
+import java.io.File;
+
+public class FileUtilsStatic {
+	public static void deleteAllDepFiles(String dir) {
+		try {
+			File[] tfiles = new File(dir).listFiles();
+			for (File aFile : tfiles) {
+				if (aFile.getName().endsWith(".txt")) {
+					aFile.delete();
+				}
+			}
+		} catch (Exception e) {
+			//dir not exists or cannot delete the file
+			e.printStackTrace();
+		}
+	}
+}
