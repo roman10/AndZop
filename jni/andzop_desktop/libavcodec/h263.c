663b541f985d6ac69e7d99862c14942300a5af4f
roman10 <liuf0005@gmail.com>
2012-03-10 16:41:02 +0800

    dump intra-frame dependency due to MV only when MV-based approach is not used

--- jni/andzop_desktop/libavcodec/h263.c
+++ jni/andzop_desktop/libavcodec/h263.c
@@ -40,6 +40,8 @@
 #include "flv.h"
 #include "mpeg4video.h"
 
+#include "../compile.h"
+
 //#undef NDEBUG
 //#include <assert.h>
 
@@ -348,20 +350,24 @@ int16_t *h263_pred_motion_dep(MpegEncContext * s, int block, int dir,
                 if(s->mb_x==0){
                     *px = C[0];
                     *py = C[1];
+#ifndef MV_BASED_DEPENDENCY
 		    if (s->mb_y > 0) {
 		    	fprintf(s->avctx->g_intraDepF, "%d:%d:", s->mb_y-1, s->mb_x+1);
 		    }
+#endif
 		    //printf("###%d:%d:%d:%d:\n", s->mb_y, s->mb_x, s->mb_y-1, s->mb_x +1);
 	            //printf("@@@%d:%d:\n%d:%d:%d:%d:\n", s->mb_y, s->mb_x, *px, *py, C[0], C[1]);
                 }else{
                     *px = mid_pred(A[0], 0, C[0]);
                     *py = mid_pred(A[1], 0, C[1]);
+#ifndef MV_BASED_DEPENDENCY
 		    if (s->mb_x > 0) {
 			fprintf(s->avctx->g_intraDepF, "%d:%d:", s->mb_y, s->mb_x-1);
 		    }
 		    if (s->mb_y > 0) {
 		    	fprintf(s->avctx->g_intraDepF, "%d:%d:", s->mb_y-1, s->mb_x+1);
 		    }
+#endif
 		    //printf("###%d:%d:%d:%d:%d:%d:\n", s->mb_y, s->mb_x, s->mb_y, s->mb_x-1, s->mb_y-1, s->mb_x+1);
                     //printf("@@@%d:%d:\n%d:%d:%d:%d:%d:%d:\n", s->mb_y, s->mb_x, *px, *py, A[0], A[1], C[0], C[1]);
                 }
@@ -372,9 +378,11 @@ int16_t *h263_pred_motion_dep(MpegEncContext * s, int block, int dir,
                 *px = A[0];
                 *py = A[1];
 		//printf("block = 0; *px = %d; *py = %d\n", *px, *py);
+#ifndef MV_BASED_DEPENDENCY
 		if (s->mb_x > 0) {
 		    fprintf(s->avctx->g_intraDepF, "%d:%d:", s->mb_y, s->mb_x-1);
 		}
+#endif
 		//printf("###%d:%d:%d:%d:\n", s->mb_y, s->mb_x, s->mb_y, s->mb_x-1);
                 //printf("@@@%d:%d:\n%d:%d:%d:%d:\n", s->mb_y, s->mb_x, *px, *py, A[0], A[1]);
             }
@@ -384,9 +392,11 @@ int16_t *h263_pred_motion_dep(MpegEncContext * s, int block, int dir,
                 C = mot_val[off[block] - wrap];
                 *px = mid_pred(A[0], 0, C[0]);
                 *py = mid_pred(A[1], 0, C[1]);
+#ifndef MV_BASED_DEPENDENCY
 		if (s->mb_y > 0) {
 		    fprintf(s->avctx->g_intraDepF, "%d:%d:", s->mb_y-1, s->mb_x+1);
 		}
+#endif
 		//printf("block = 1; s->mb_x + 1 == s->resync_mb_x && s->h263_pred; *px = %d; *py = %d\n", *px, *py);
 	        //printf("###x=%d, y=%d, %d:%d:%d:%d:\n", *px, *py, s->mb_y, s->mb_x, s->mb_y-1, s->mb_x+1);
                 //printf("@@@%d:%d:\n%d:%d:%d:%d:%d:%d:\n", s->mb_y, s->mb_x, *px, *py, A[0], A[1], C[0], C[1]);
@@ -407,6 +417,7 @@ int16_t *h263_pred_motion_dep(MpegEncContext * s, int block, int dir,
             *px = mid_pred(A[0], B[0], C[0]);
             *py = mid_pred(A[1], B[1], C[1]);
 	    //printf("@@@%d:%d:\n%d:%d:%d:%d:%d:%d:%d:%d:\n", s->mb_y, s->mb_x, *px, *py, A[0], A[1], B[0], B[1], C[0], C[1]);
+#ifndef MV_BASED_DEPENDENCY
 	    if(s->mb_x == s->resync_mb_x) {
                 //printf("###%d:%d:\n", s->mb_y, s->mb_x);    
             } else {
@@ -415,6 +426,7 @@ int16_t *h263_pred_motion_dep(MpegEncContext * s, int block, int dir,
 		}
                 //printf("###%d:%d:%d:%d:\n", s->mb_y, s->mb_x, s->mb_y, s->mb_x-1);    
             }
+#endif
         }
     } else {
 	//this corresponds to figure 7-20 diagram a, b, c and d, based on block value
@@ -429,6 +441,7 @@ int16_t *h263_pred_motion_dep(MpegEncContext * s, int block, int dir,
         *py = mid_pred(A[1], B[1], C[1]);
         //printf("@@@%d:%d:\n%d:%d:%d:%d:%d:%d:%d:%d:\n", s->mb_y, s->mb_x, *px, *py, A[0], A[1], B[0], B[1], C[0], C[1]);
         //feipeng: added to track motion vector differential encoding dependency
+#ifndef MV_BASED_DEPENDENCY
 	if (block == 0) {
 	    if (s->mb_x > 0) {
 		fprintf(s->avctx->g_intraDepF, "%d:%d:", s->mb_y, s->mb_x-1);
@@ -438,19 +451,20 @@ int16_t *h263_pred_motion_dep(MpegEncContext * s, int block, int dir,
    	    }
 	    
             //printf("###%d:%d:%d:%d:%d:%d:%d:%d:\n", s->mb_y, s->mb_x, s->mb_y, s->mb_x-1, s->mb_y-1, s->mb_x, s->mb_y-1, s->mb_x+1);
-        } else if (block == 1) {
+    } else if (block == 1) {
 	    if (s->mb_y > 0) {
    	        fprintf(s->avctx->g_intraDepF, "%d:%d:%d:%d:", s->mb_y-1, s->mb_x, s->mb_y-1, s->mb_x+1);
 	    }
             //printf("###%d:%d:%d:%d:%d:%d:\n", s->mb_y, s->mb_x, s->mb_y-1, s->mb_x, s->mb_y-1, s->mb_x+1);
-        } else if (block == 2) {
+    } else if (block == 2) {
 	    if (s->mb_x > 0) {
 	        fprintf(s->avctx->g_intraDepF, "%d:%d:", s->mb_y, s->mb_x - 1);
 	    }
             //printf("###%d:%d:%d:%d:\n", s->mb_y, s->mb_x, s->mb_y, s->mb_x - 1);
-        } else if (block == 3) {
+    } else if (block == 3) {
             //printf("###%d:%d:\n", s->mb_y, s->mb_x);
         } 
+#endif
     }
     return *mot_val;
 }
